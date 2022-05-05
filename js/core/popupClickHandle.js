9afb18e5f3aa809f83539211d1d222f59bab23f1
abdecodes <96370319+abdecodes@users.noreply.github.com>
2022-05-05 20:15:12 +0530

    Delete js directory

--- js/core/popupClickHandle.js
+++
@@ -1,9 +0,0 @@
-
-	document.getElementById('merge-pdf-button').addEventListener('click',() => {    
-	     showPDFMerger();
-	 window.close(); 
-	});
-	document.getElementById('meta-editor-button').addEventListener('click',() => {    
-	     metaDataEditor();
-	 window.close(); 
-	});
